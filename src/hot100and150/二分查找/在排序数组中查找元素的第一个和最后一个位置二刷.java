8b4949ffcffc412f41559a3762a10ec1fd3c7c6f
hanxiaoge <1072407118@qq.com>
2025-02-27 11:55:15 +0800

    2.27

---
+++ src/hot100and150/二分查找/在排序数组中查找元素的第一个和最后一个位置二刷.java
@@ -0,0 +1,51 @@
+package hot100and150.二分查找;
+
+/**
+ * @author hxg
+ * @description: 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+ * <p>
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ * <p>
+ * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+ * @date 2025/2/27 09:18
+ */
+public class 在排序数组中查找元素的第一个和最后一个位置二刷 {
+    public int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+
+        int left = findLeft(nums, target);
+        if (left == -1) {
+            return new int[]{-1, -1};
+        }
+        int right = findRight(nums, target);
+        return new int[]{left, right};
+    }
+
+    private int findLeft(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left] == target ? left : -1;
+    }
+
+    private int findRight(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2; // 向上取整避免死循环
+            if (nums[mid] <= target) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return nums[left] == target ? left : -1;
+    }
+}
