89000b48b479fd591ab9b2e724bc22fe8f121e67
hanxiaoge08 <1072407118@qq.com>
2025-06-10 09:55:12 +0800

    6

--- src/hot100second/动态规划/最长回文子串.java
+++ src/hot100second/动态规划/最长回文子串.java
@@ -48,22 +48,22 @@ public class 最长回文子串 {
         return s.substring(begin,begin+maxLen);
 
     }
-    public String longestPalindrome1(String s){
-        if(s==null||s.isEmpty()){
-            return "";
-        }
-        int start=0,end=0;
-        for(int i=0;i<s.length();i++){
-            int len1=expandAroundCenter(s,i,i);
-            int len2=expandAroundCenter(s,i,i+1);
-            int len=Math.max(len1,len2);
-            if(len>end-start){
-                start=i-(len-1)/2;
-                end=i+len/2;
-            }
-        }
-        return s.substring(start,end+1);
-    }
+//    public String longestPalindrome1(String s){
+//        if(s==null||s.isEmpty()){
+//            return "";
+//        }
+//        int start=0,end=0;
+//        for(int i=0;i<s.length();i++){
+//            int len1=expandAroundCenter(s,i,i);
+//            int len2=expandAroundCenter(s,i,i+1);
+//            int len=Math.max(len1,len2);
+//            if(len>end-start){
+//                start=i-(len-1)/2;
+//                end=i+len/2;
+//            }
+//        }
+//        return s.substring(start,end+1);
+//    }
     public int expandAroundCenter(String s,int left,int right){
         while (left>0&&right<s.length()&&s.charAt(left)==s.charAt(right)){
             left--;
