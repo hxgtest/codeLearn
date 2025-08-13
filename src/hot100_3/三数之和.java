a94d7064d7eb2046fd60f322d534f070b807d43f
hanxiaoge <1072407118@qq.com>
2025-08-13 09:27:27 +0800

    feat(hot100_3): 添加三数之和和接雨水问题的解决方案
    - 新增三数之和类，实现三数之和为零的所有不重复组合
    - 新增接雨水类，计算并返回 trapped 的雨水总量

---
+++ src/hot100_3/三数之和.java
@@ -0,0 +1,47 @@
+package hot100_3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author hxg
+ * @description: TODO
+ * @date 2025/8/11 22:35
+ */
+public class 三数之和 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n=nums.length;
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums);
+        if(nums[0]>0){
+            return ans;
+        }
+        for(int i=0;i<n;i++){
+            if(i>0&&nums[i]==nums[i-1]){
+                continue;
+            }
+            int third =n-1;
+            int target=-nums[i];
+            for(int second=i+1;second<n;second++){
+                if(second>i+1&&nums[second]==nums[second-1]){
+                    continue;
+                }
+                while (second< third &&nums[second]+nums[third]>target){
+                    third--;
+                }
+                if(second==third){
+                    break;
+                }
+                if(nums[second]+nums[third]==target){
+                    List<Integer> list=new ArrayList<>();
+                    list.add(nums[i]);list.add(nums[second]);list.add(nums[third]);
+                    ans.add(list);
+                }
+
+            }
+        }
+        return ans;
+    }
+    
+}
