f4a6ee54c96ce6830b9960153a3eaca21626eb6a
hanxiaoge <1072407118@qq.com>
2025-08-13 10:05:02 +0800

    feat(hot100_3): 添加无重复字符的最长子串问题的Java解决方案
    - 新增一个名为"无重复字符的最长子串"的Java类- 提供了一个方法lengthOfLongestSubstring来解决该问题
    - 目前方法内部返回0，需要进一步实现具体逻辑

---
+++ src/hot100_3/无重复字符的最长子串.java
@@ -0,0 +1,13 @@
+package hot100_3;
+
+/**
+ * @author hxg
+ * @description: TODO
+ * @date 2025/8/12 22:15
+ */
+public class 无重复字符的最长子串 {
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+
+}
