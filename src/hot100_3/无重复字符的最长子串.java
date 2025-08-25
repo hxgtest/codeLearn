45111d100df16adac5dc19a8fc4d2ee4964e4298
hanxiaoge <1072407118@qq.com>
2025-08-25 21:27:04 +0800

    8.25

--- src/hot100_3/无重复字符的最长子串.java
+++ src/hot100_3/无重复字符的最长子串.java
@@ -1,5 +1,10 @@
 package hot100_3;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @author hxg
  * @description: TODO
@@ -7,7 +12,24 @@ package hot100_3;
  */
 public class 无重复字符的最长子串 {
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+        int max=0;
+        int left=0,right=0;
+        int n=s.length();
+        char[] chars=s.toCharArray();
+        Set<Character> map=new HashSet<>();
+        while (right<n){
+            if(map.contains(chars[right])){
+                while (left<=right&&map.contains(chars[right])){
+                    map.remove(chars[left]);
+                    left++;
+                }
+            }else {
+                map.add(chars[right]);
+                right++;
+                max=Math.max(max,right-left);
+            }
+        }
+        return max;
     }
 
 }
